39dd3ed7d72823b6173aa4916fe6de3e2eb2eeb6
Billy <bllyanos@gmail.com>
2020-09-11 17:50:02 +0700

    useTimer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const [inputTime, setInputTime] = useState(0);
+  const [time, isCounting, startTimer] = useTimer();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {"IS COUNTING: " + isCounting}
+        <br />
+        {time}
+        <div>
+          <input type="number" value={inputTime} onChange={e => setInputTime(Number(e.target.value) || 0)} />
+          <button onClick={() => {
+            startTimer(inputTime)
+          }}>startTime</button>
+        </div>
       </header>
     </div>
   );
+}
+
+function useTimer(): [number, boolean, (v: number) => void] {
+  const [time, setTime] = useState(0);
+
+  const [isCounting, setIsCounting] = useState(false);
+  const [theInterval, setTheInterval] = useState<any>(null);
+
+  const startTimer = useCallback((time: number) => {
+    if (isCounting) {
+      clearInterval(theInterval);
+      setTheInterval(null);
+    }
+    setTime(time);
+    setIsCounting(true);
+    setTheInterval(setInterval(() => {
+      setTime(c => c - 1);
+    }, 1000));
+  }, [isCounting]);
+
+  useEffect(() => {
+    if (time === 0) {
+      clearInterval(theInterval);
+      setTheInterval(null);
+      setIsCounting(false);
+    }
+  }, [time]);
+
+  return [time, isCounting, startTimer];
 }
 
 export default App;
